cdf241a991bd1c699c545a9da917e19b5b6baf99
Alexander Downie <alexander-downie@sky.com>
2023-02-20 16:47:35 +0000

    Remove debug line

--- index.js
+++ index.js
@@ -142,7 +142,6 @@ async function getTicketStatuses() {
 
             //Uses card structure
             if($(el).find(".eds-card").length > 0){
-                console.log("Using card structure");
                 ticketTitle = $(el).find(".eds-card .ticket-display-card-content-full-size__ticket-name").text();
 
                 //Set default status
